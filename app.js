436fb352c8e460113d91d0bd73f88a7d5377fe8a
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-10-10 13:12:22 +0545

    Data post method applied

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const connectToDataBase = require('./Database/index.js')
 
 const app = express();
+app.use(express.json())
 
 connectToDataBase();
 // Define routes
@@ -13,11 +14,14 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/about', (req, res) => {
-    res.status(200).json({
-        message: "This is the about page"
-    });
-});
+app.post('/blog', (req, res) => {
+console.log(req.body);
+res.status(200).json({
+    message : "Blog successful"
+})
+
+})
+
 
 // Start the server
 app.listen(process.env.PORT, () => {
