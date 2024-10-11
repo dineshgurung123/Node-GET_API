fdbbbe67f3727c51f2f576ef347d6e3447e0dd10
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-10-11 15:14:21 +0545

    Data inserted in cloud database

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 require ('dotenv').config();
 const express = require('express');
-const connectToDataBase = require('./Database/index.js')
+const connectToDataBase = require('./Database/index.js');
+const Blog = require('./model/blogModel.js');
 
 const app = express();
 app.use(express.json())
@@ -14,8 +15,34 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/blog', (req, res) => {
-console.log(req.body);
+app.post('/blog', async(req, res) => {
+// const title = req.body.title
+// const subtitle = req.body.subtitle
+// const description = req.body.description
+// const image = req.body.image
+
+
+const {title, subtitle, description, image} = req.body
+ 
+
+
+if(!title || !description || !image || !subtitle){
+
+    return res.status(400).json({
+        message:"Please provide all data"
+    })
+}
+
+await Blog.create({
+   title : title,
+   subtitle : subtitle,
+   description : description,
+   image : image
+
+    
+})
+
+
 res.status(200).json({
     message : "Blog successful"
 })
--- model/blogModel.js
+++ model/blogModel.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose')
     },
 
     description : {
-     type : Text
+     type : String
 
     },
 
