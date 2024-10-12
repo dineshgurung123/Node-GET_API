90b975e3addae4193afa29abc5fe30a88fbe22df
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-10-12 15:38:27 +0545

    middleware multer added

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const express = require('express');
 const connectToDataBase = require('./Database/index.js');
 const Blog = require('./model/blogModel.js');
 
+
+
 const app = express();
 app.use(express.json())
+const {multer, storage} = require('./middleware/multerConfig.js')
+
+const upload = multer({storage : storage})
 
 connectToDataBase();
 // Define routes
@@ -15,16 +20,14 @@ app.get('/', (req, res) => {
     });
 });
 
-app.post('/blog', async(req, res) => {
+app.post('/blog', upload.single('image') ,async(req, res) => {
 // const title = req.body.title
 // const subtitle = req.body.subtitle
 // const description = req.body.description
 // const image = req.body.image
 
-
-const {title, subtitle, description, image} = req.body
- 
-
+const {title, subtitle, description} = req.body
+const image = req.file
 
 if(!title || !description || !image || !subtitle){
 
@@ -33,15 +36,25 @@ if(!title || !description || !image || !subtitle){
     })
 }
 
-await Blog.create({
-   title : title,
-   subtitle : subtitle,
-   description : description,
-   image : image
-
+try {
+    await Blog.create({
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : image.path
+     
+         
+     })
+     
+} catch (error) {
     
-})
+    if (error.code === 11000) {
+        return res.status(400).json({
+            message: "Blog title already exists. Please choose a different title."
+        });
+    }
 
+}
 
 res.status(200).json({
     message : "Blog successful"
---
+++ middleware/multerConfig.js
@@ -0,0 +1,20 @@
+const multer = require('multer')
+
+ const storage = multer.diskStorage({
+
+    destination : function (req, file, cb) {
+        
+        cb(null, './storage')//cb(error, success)
+    },
+
+     filename : function (req, file, cb){
+
+    cb(null, "Dinesh-"+ file.originalname)    
+     }
+
+})
+
+module.exports = { 
+    multer, 
+    storage
+}
