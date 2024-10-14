a94b856e13631c675681ead72933b1b6cf9e4b3b
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-10-14 22:19:25 +0545

    code updated

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.json())
 const {multer, storage} = require('./middleware/multerConfig.js')
 
 const upload = multer({storage : storage})
+const fs = require('fs');
+
 
 connectToDataBase();
 // Define routes
@@ -80,6 +82,69 @@ data : blogs
  })
 })
 
+
+app.get("/blog/:id", async(req, res)=>{
+
+const id = req.params.id;
+ const blog = await Blog.findById(id); //object
+if (!blog) {
+    
+   return res.status(404).json({
+        message: "No data found"
+    })
+}
+else{
+
+ res.status(200).json({
+
+    message : "fetched successfully",
+    data : blog
+ })
+
+}
+})
+
+app.delete("/blog/:id", async(req,res)=>{
+
+   const id = req.params.id
+
+   await Blog.findByIdAndDelete(id)
+   fs.unlink('storage/a.png', (err)=>{
+    
+    if(err){
+
+        console.log(err);
+    }
+    else{
+        console.log('file deleted');
+    }
+
+   })    
+
+   res.status(200).json({
+
+message : "Blog deleted sucessfully"
+
+   })
+})
+
+
+app.patch('/blog/:id', async(req,res)=>{
+const id = req.params.id 
+const {title, subtitle, description} = req.body
+ await Blog.findByIdAndUpdate(id, {
+
+    title : title,
+    subtitle : subtitle,
+    description : description
+})
+res.status(200).json({
+
+    message : "Blog updated successfully"
+})
+
+
+})
 
 // Start the server
 app.listen(process.env.PORT, () => {
